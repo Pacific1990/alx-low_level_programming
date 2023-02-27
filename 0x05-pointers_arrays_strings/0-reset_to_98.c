a34b4bbd01a5fd254bf5ebaefccf69f1b87b2d65
Eko Abet <cpntic.etfp@gmail.com>
2023-02-27 17:05:43 +0000

    Commit 002

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,5 @@
-/*
- * File: 0-reset_to_98.c
- * Auth: Brennan D Baraban
- */
 #include "main.h"
 
-/**
- *rest_to_98 - updates a integer value to 98
- *@n: A pointer to an int that will be updated
- *
- *Return: void that means is correct
- */
 void reset_to_98(int *n)
 {
 *n = 98;
